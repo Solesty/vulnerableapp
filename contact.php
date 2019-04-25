f444b549ec7e0c1f920e888d6f286c726aafeb3c
ABOYEJI SOLOMON ADELEKE <solesty7@gmail.com>
2019-04-25 12:01:36 +0100

    Changed name from SureBank MF to SureMFB

--- contact.php
+++ contact.php
@@ -42,7 +42,7 @@
         <span></span>
         <span></span>
       </button>
-      <a class="navbar-brand text-brand" href="">Sure<span class="color-b">Bank MF</span></a>
+      <a class="navbar-brand text-brand" href="">Sure<span class="color-b">MFB</span></a>
       <button type="button" class="btn btn-link nav-search navbar-toggle-box-collapse d-md-none" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-expanded="false">
         <span class="fa fa-search" aria-hidden="true"></span>
       </button>
